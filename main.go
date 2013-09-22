887faadb65c241e28c4870456ae74700d0b262bc
Marin <mgs@jpisoftware.net>
2013-09-22 16:34:23 -0700

    minor cleanup

--- main.go
+++ main.go
@@ -55,23 +55,27 @@ func PlayPause() {
 }
 
 func CurSong() {
-	sdata := new(dbus.Variant)
-	// playing status
-	pstatus := new(dbus.Variant)
+	sdata := new(dbus.Variant)				// song data
+	pstatus := new(dbus.Variant)			// playing status
+
 	sdbus := connDbus()
-	sdbus.Call("Get", 0, "org.mpris.MediaPlayer2.Player","Metadata").Store(sdata)
-	err := sdbus.Call("Get", 0, "org.mpris.MediaPlayer2.Player","PlaybackStatus").Store(pstatus)
+
+	// get song data, quit on err
+	err := sdbus.Call("Get", 0, "org.mpris.MediaPlayer2.Player","Metadata").Store(sdata)
 
 	if err != nil {
 		// most likely spotify not running
 		return
 	}
 
+	sdbus.Call("Get", 0, "org.mpris.MediaPlayer2.Player","PlaybackStatus").Store(pstatus)
+
+
 	songData := sdata.Value().(map[string]dbus.Variant)
 
-	title := songData["xesam:title"]
 	// buggy spotify dbus only sends a single artist
 	artist := songData["xesam:artist"].Value().([]string)
+	title := songData["xesam:title"]
 	rating := int(songData["xesam:autoRating"].Value().(float64) * 100)
 
 	if songStatus := pstatus.Value().(string); songStatus == "Paused" {
