724df70cd04a39ee51e2e41c969f68f0f28f1aa4
Marin <mgs@jpisoftware.net>
2013-10-22 14:58:15 -0700

    add album download with -i option

--- main.go
+++ main.go
@@ -1,23 +1,34 @@
 package main
 
 import (
-	//dbus "../go.dbus"
-	dbus "github.com/hoffoo/go.dbus"
 	"flag"
 	"fmt"
-	//"os"
+	dbus "github.com/hoffoo/go.dbus"
+	"io"
+	"net/http"
+	"os"
+	"os/user"
 	"strings"
 )
 
 var sdbus *dbus.Object
 
-const ART_CACHE = "~/.spotify-art/"
+const ART_BASE = "/.spotify-art/"
+var ART_CACHE string
 
 func main() {
 
 	var img bool
 	flag.BoolVar(&img, "i", false, "get album art image")
 	flag.Parse()
+
+	if img {
+		u, err := user.Current()
+		if err != nil{
+			panic(err)
+		}
+		ART_CACHE = u.HomeDir+ART_BASE
+	}
 
 	action := flag.Arg(0)
 
@@ -37,6 +48,11 @@ func main() {
 			fmt.Printf("(paused) %s %s (paused)", artist[0], title)
 		} else {
 			fmt.Printf("%s %s (%d)", artist[0], title, rating)
+		}
+
+		if img == true {
+			artUrl := songData["mpris:artUrl"].Value().(string)
+			Art(artUrl)
 		}
 	case "url":
 		song := Metadata()
@@ -97,9 +113,26 @@ func Status() *dbus.Variant {
 	return &pstatus
 }
 
-//func FetchArt(url string) {
-//
-//	idx := strings.LastIndex(url, "/")
-//	filename := url[idx+1:]
-//
-//}
+func Art(url string) {
+
+	idx := strings.LastIndex(url, "/")
+	filename := url[idx+1:]
+
+	outfile, err := os.OpenFile(ART_CACHE+filename, os.O_RDONLY, 0660)
+
+	if os.IsNotExist(err) {
+		resp, err := http.Get("http://open.spotify.com/image/" + filename)
+		defer resp.Body.Close()
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "couldnt download album art")
+			return
+		}
+
+		outfile, err = os.Create(ART_CACHE+filename)
+
+		io.Copy(outfile, resp.Body)
+	}
+
+	os.Link(ART_CACHE+filename, ART_CACHE+"cur")
+}
